3c0d8f6a186fa95c4ddf8aa04bdbc82b43a4f929
StudentBI18 <StudentBI18@SYN13ASS-NB-547.alabuga.local>
2024-12-28 17:18:44 +0300

    Editet: Main page   add: styles for Main Page

--- project/project/settings.py
+++ project/project/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = [
+    'localhost', 
+    '127.0.0.1', 
+    '10.14.36.31',
+]
